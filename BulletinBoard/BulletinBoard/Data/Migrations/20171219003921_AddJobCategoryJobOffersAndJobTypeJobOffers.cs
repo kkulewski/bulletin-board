7d8c57df7e2ba16cc66712ab6588a3916d35d0c4
Krzysztof Kulewski <krzysztofkulewski@gmail.com>
2017-12-19 01:41:42 +0100

    Add job offers collection to category and type

---
+++ BulletinBoard/BulletinBoard/Data/Migrations/20171219003921_AddJobCategoryJobOffersAndJobTypeJobOffers.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using System;
+using System.Collections.Generic;
+
+namespace BulletinBoard.Data.Migrations
+{
+    public partial class AddJobCategoryJobOffersAndJobTypeJobOffers : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
--- BulletinBoard/BulletinBoard/Models/JobCategory.cs
+++ BulletinBoard/BulletinBoard/Models/JobCategory.cs
@@ -1,9 +1,13 @@
-﻿namespace BulletinBoard.Models
+﻿using System.Collections.Generic;
+
+namespace BulletinBoard.Models
 {
     public class JobCategory
     {
         public string JobCategoryId { get; set; }
 
         public string Name { get; set; }
+
+        public ICollection<JobOffer> JobOffers { get; set; }
     }
 }
