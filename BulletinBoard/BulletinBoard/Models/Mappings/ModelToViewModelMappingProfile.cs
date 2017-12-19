07a83accb4239a06d8de237e1d4c97f88dc36edb
Krzysztof Kulewski <krzysztofkulewski@gmail.com>
2017-12-19 23:59:34 +0100

    Add automapper model to vm mapping profile

---
+++ BulletinBoard/BulletinBoard/Models/Mappings/ModelToViewModelMappingProfile.cs
@@ -0,0 +1,16 @@
+﻿using AutoMapper;
+using BulletinBoard.Models.JobTypeViewModels;
+
+namespace BulletinBoard.Models.Mappings
+{
+    public class ModelToViewModelMappingProfile : Profile
+    {
+        public ModelToViewModelMappingProfile()
+        {
+            CreateMap<CreateJobTypeViewModel, JobType>();
+            CreateMap<DeleteJobTypeViewModel, JobType>();
+            CreateMap<DetailsJobTypeViewModel, JobType>();
+            CreateMap<EditJobTypeViewModel, JobType>();
+        }
+    }
+}
