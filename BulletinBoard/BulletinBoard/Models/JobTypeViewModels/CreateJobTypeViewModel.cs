a4a88f0c9cd97751eecf5d201c37fc00dc4ff3f1
Krzysztof Kulewski <krzysztofkulewski@gmail.com>
2017-12-19 22:38:29 +0100

    Add create job type vm constraints

--- BulletinBoard/BulletinBoard/Models/JobTypeViewModels/CreateJobTypeViewModel.cs
+++ BulletinBoard/BulletinBoard/Models/JobTypeViewModels/CreateJobTypeViewModel.cs
@@ -1,7 +1,12 @@
-﻿namespace BulletinBoard.Models.JobTypeViewModels
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BulletinBoard.Models.JobTypeViewModels
 {
     public class CreateJobTypeViewModel
     {
+        [Required]
+        [StringLength(100, ErrorMessage = "The {0} must be at least {2} and at max {1} characters long.", MinimumLength = 3)]
+        [Display(Name = "Name")]
         public string Name { get; set; }
     }
 }
