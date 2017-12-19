c545e31b934863186f9f382d91e2f29d4347e68d
Krzysztof Kulewski <krzysztofkulewski@gmail.com>
2017-12-19 03:40:21 +0100

    Add roles helper

---
+++ BulletinBoard/BulletinBoard/Helpers/Roles.cs
@@ -0,0 +1,14 @@
+﻿namespace BulletinBoard.Helpers
+{
+    public static class Roles
+    {
+        public const string Administrator = "Administrator";
+        public const string Moderator = "Moderator";
+        public const string User = "User";
+
+        public static string Normalize(string roleName)
+        {
+            return roleName.ToUpper();
+        }
+    }
+}
