b38d40cc38d9d71ae18436c6a4394d4ffe7091ed
Krzysztof Kulewski <krzysztofkulewski@gmail.com>
2017-12-20 01:32:08 +0100

    Add Html.DisplayFor tag helper alternative

---
+++ BulletinBoard/BulletinBoard/Helpers/TagHelpers/DisplayForTagHelper.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using Microsoft.AspNetCore.Mvc.ViewFeatures;
+using Microsoft.AspNetCore.Razor.TagHelpers;
+
+namespace BulletinBoard.Helpers.TagHelpers
+{
+    [HtmlTargetElement("p", Attributes = ForAttributeName)]
+    public class DisplayForTagHelper : TagHelper
+    {
+        private const string ForAttributeName = "asp-display-for";
+
+        [HtmlAttributeName(ForAttributeName)]
+        public ModelExpression For { get; set; }
+
+        public override void Process(TagHelperContext context, TagHelperOutput output)
+        {
+            if (context == null)
+            {
+                throw new ArgumentNullException(nameof(context));
+            }
+
+            if (output == null)
+            {
+                throw new ArgumentNullException(nameof(output));
+            }
+
+            var text = For.ModelExplorer.GetSimpleDisplayText();
+            output.Content.SetContent(text);
+        }
+    }
+}
