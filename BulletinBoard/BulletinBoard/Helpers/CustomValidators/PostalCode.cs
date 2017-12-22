c446025f3444c37875038242a74aee581dfe1430
Krzysztof Kulewski <krzysztofkulewski@gmail.com>
2017-12-22 22:40:40 +0100

    Add postal code validator

---
+++ BulletinBoard/BulletinBoard/Helpers/CustomValidators/PostalCode.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Text.RegularExpressions;
+
+namespace BulletinBoard.Helpers.CustomValidators
+{
+    public class PostalCode : ValidationAttribute
+    {
+        protected override ValidationResult IsValid(object value, ValidationContext validationContext)
+        {
+            if (value == null)
+            {
+                return new ValidationResult(validationContext.DisplayName + "is required.");
+            }
+
+            var postalCode = value.ToString();
+            if (Regex.IsMatch(postalCode, "^[0-9]{2}[-][0-9]{3}$"))
+            {
+                return ValidationResult.Success;
+            }
+
+            return new ValidationResult("Invalid post code (use format: xx-xxx).");
+        }
+    }
+}
