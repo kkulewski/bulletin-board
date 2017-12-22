ce0b5c736231c4fe5a347b4384221a700830cec4
Krzysztof Kulewski <krzysztofkulewski@gmail.com>
2017-12-22 03:03:54 +0100

    Add job offer category + type mappings, fix mappings - change source with destination

--- BulletinBoard/BulletinBoard/Models/Mappings/ModelToViewModelMappingProfile.cs
+++ BulletinBoard/BulletinBoard/Models/Mappings/ModelToViewModelMappingProfile.cs
@@ -9,23 +9,31 @@ namespace BulletinBoard.Models.Mappings
     {
         public ModelToViewModelMappingProfile()
         {
-            CreateMap<JobTypeViewModel, JobType>();
-            CreateMap<CreateJobTypeViewModel, JobType>();
-            CreateMap<DeleteJobTypeViewModel, JobType>();
-            CreateMap<DetailsJobTypeViewModel, JobType>();
-            CreateMap<EditJobTypeViewModel, JobType>();
+            CreateMap<JobType, JobTypeViewModel>();
+            CreateMap<JobType, CreateJobTypeViewModel>();
+            CreateMap<JobType, DeleteJobTypeViewModel>();
+            CreateMap<JobType, DetailsJobTypeViewModel>();
+            CreateMap<JobType, EditJobTypeViewModel>();
 
-            CreateMap<JobCategoryViewModel, JobCategory>();
-            CreateMap<CreateJobCategoryViewModel, JobCategory>();
-            CreateMap<DeleteJobCategoryViewModel, JobCategory>();
-            CreateMap<DetailsJobCategoryViewModel, JobCategory>();
-            CreateMap<EditJobCategoryViewModel, JobCategory>();
+            CreateMap<JobCategory, JobCategoryViewModel>();
+            CreateMap<JobCategory, CreateJobCategoryViewModel>();
+            CreateMap<JobCategory, DeleteJobCategoryViewModel>();
+            CreateMap<JobCategory, DetailsJobCategoryViewModel>();
+            CreateMap<JobCategory, EditJobCategoryViewModel>();
 
-            CreateMap<JobOfferViewModel, JobCategory>();
-            CreateMap<CreateJobOfferViewModel, JobCategory>();
-            CreateMap<DeleteJobOfferViewModel, JobCategory>();
-            CreateMap<DetailsJobOfferViewModel, JobCategory>();
-            CreateMap<EditJobOfferViewModel, JobCategory>();
+            CreateMap<JobOffer, JobOfferViewModel > ();
+            CreateMap<JobOffer, CreateJobOfferViewModel>()
+                .ForMember(dest => dest.JobCategoryId,
+                    opts => opts.MapFrom(src => src.JobCategory.JobCategoryId))
+                .ForMember(dest => dest.JobTypeId,
+                    opts => opts.MapFrom(src => src.JobType.JobTypeId));
+            CreateMap<JobOffer, DeleteJobOfferViewModel>();
+            CreateMap<JobOffer, DetailsJobOfferViewModel>();
+            CreateMap<JobOffer, EditJobOfferViewModel>()
+                .ForMember(dest => dest.JobCategoryId,
+                    opts => opts.MapFrom(src => src.JobCategory.JobCategoryId))
+                .ForMember(dest => dest.JobTypeId,
+                    opts => opts.MapFrom(src => src.JobType.JobTypeId));
         }
     }
 }
