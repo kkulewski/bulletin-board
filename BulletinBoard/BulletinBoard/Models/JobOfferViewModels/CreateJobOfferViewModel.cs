4312d9ed1304f0a61b2a022bcdc40bc5863cb160
Krzysztof Kulewski <krzysztofkulewski@gmail.com>
2017-12-22 01:23:14 +0100

    Add job offer vm constraints

--- BulletinBoard/BulletinBoard/Models/JobOfferViewModels/CreateJobOfferViewModel.cs
+++ BulletinBoard/BulletinBoard/Models/JobOfferViewModels/CreateJobOfferViewModel.cs
@@ -1,17 +1,32 @@
-﻿namespace BulletinBoard.Models.JobOfferViewModels
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BulletinBoard.Models.JobOfferViewModels
 {
     public class CreateJobOfferViewModel
     {
         public string JobOfferId { get; set; }
 
+        [Required]
+        [Display(Name = "Category")]
         public JobCategory JobCategory { get; set; }
 
+        [Required]
+        [Display(Name = "Type")]
         public JobType JobType { get; set; }
 
+        [Required]
+        [StringLength(100, ErrorMessage = "The {0} must be at least {2} and at max {1} characters long.", MinimumLength = 5)]
+        [Display(Name = "Title")]
         public string Title { get; set; }
 
+        [Required]
+        [StringLength(100, ErrorMessage = "The {0} must be at least {2} and at max {1} characters long.", MinimumLength = 50)]
+        [Display(Name = "Description")]
         public string Description { get; set; }
 
+        [Required]
+        [DataType(DataType.Currency)]
+        [Display(Name = "Wage")]
         public decimal Wage { get; set; }
     }
 }
