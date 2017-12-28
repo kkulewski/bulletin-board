51a5f926129aa32d9ea891e44a5af007314c3d2f
Krzysztof Kulewski <krzysztofkulewski@gmail.com>
2017-12-28 21:21:19 +0100

    Add hidden email tag helper

---
+++ BulletinBoard/BulletinBoard/Helpers/TagHelpers/HiddenEmailTagHelper.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Linq;
+using Microsoft.AspNetCore.Mvc.ViewFeatures;
+using Microsoft.AspNetCore.Razor.TagHelpers;
+
+namespace BulletinBoard.Helpers.TagHelpers
+{
+    [HtmlTargetElement("p", Attributes = ForAttributeName)]
+    public class HiddenEmailTagHelper : TagHelper
+    {
+        private const string ForAttributeName = "asp-hidden-email";
+
+        [HtmlAttributeName(ForAttributeName)]
+        public ModelExpression For { get; set; }
+
+        public override void Process(TagHelperContext context, TagHelperOutput output)
+        {
+            if (context == null)
+            {
+                throw new ArgumentNullException(nameof(context));
+            }
+
+            if (output == null)
+            {
+                throw new ArgumentNullException(nameof(output));
+            }
+
+            var text = For.ModelExplorer.GetSimpleDisplayText();
+            var formatted = "...";
+
+            if (text.Length > 5)
+            {
+                var beginning = text.Substring(0, 2);
+                var ending = text.Substring(text.Length - 3, 3);
+                formatted = beginning + "..." + ending;
+            }
+
+            output.Content.SetContent(formatted);
+        }
+    }
+}
