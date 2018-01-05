f0d5d9c4e4e91ff76600c372a65f7cea9aeb832a
Krzysztof Kulewski <krzysztofkulewski@gmail.com>
2018-01-05 19:14:07 +0100

    Simplify db context

--- BulletinBoard/BulletinBoard/Data/ApplicationDbContext.cs
+++ BulletinBoard/BulletinBoard/Data/ApplicationDbContext.cs
@@ -6,22 +6,14 @@ namespace BulletinBoard.Data
 {
     public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
     {
+        // declare each database entity here to access it with dbContext
         public virtual DbSet<ApplicationUser> ApplicationUsers { get; set; }
         public virtual DbSet<JobCategory> JobCategories { get; set; }
         public virtual DbSet<JobType> JobTypes { get; set; }
         public virtual DbSet<JobOffer> JobOffers { get; set; }
 
-        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
-            : base(options)
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options): base(options)
         {
-        }
-
-        protected override void OnModelCreating(ModelBuilder builder)
-        {
-            base.OnModelCreating(builder);
-            // Customize the ASP.NET Identity model and override the defaults if needed.
-            // For example, you can rename the ASP.NET Identity table names and more.
-            // Add your customizations after calling base.OnModelCreating(builder);
         }
     }
 }
