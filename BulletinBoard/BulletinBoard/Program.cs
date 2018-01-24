15fe0972e0a4d39bfbc7262ae480849e539652e8
Krzysztof Kulewski <krzysztofkulewski@gmail.com>
2018-01-24 02:06:22 +0100

    Use default member method body for simplicity

--- BulletinBoard/BulletinBoard/Program.cs
+++ BulletinBoard/BulletinBoard/Program.cs
@@ -10,9 +10,11 @@ namespace BulletinBoard
             BuildWebHost(args).Run();
         }
 
-        public static IWebHost BuildWebHost(string[] args) =>
-            WebHost.CreateDefaultBuilder(args)
+        public static IWebHost BuildWebHost(string[] args)
+        {
+            return WebHost.CreateDefaultBuilder(args)
                 .UseStartup<Startup>()
                 .Build();
+        }
     }
 }
