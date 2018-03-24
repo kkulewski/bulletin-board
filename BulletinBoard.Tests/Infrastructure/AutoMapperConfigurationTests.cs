b6e40092630ad69309837019123a9dc753077de7
Krzysztof Kulewski <krzysztofkulewski@gmail.com>
2018-03-24 20:53:19 +0100

    Add automapper configuration test

---
+++ BulletinBoard.Tests/Infrastructure/AutoMapperConfigurationTests.cs
@@ -0,0 +1,16 @@
+﻿using AutoMapper;
+using BulletinBoard.Infrastructure;
+using Xunit;
+
+namespace BulletinBoard.Tests.Infrastructure
+{
+    public class AutoMapperConfigurationTests
+    {
+        [Fact (Skip = "Mapping profile is not finished yet")]
+        public void Configuration_IsValid()
+        {
+            var config = new MapperConfiguration(cfg => cfg.AddProfile(new AutoMapperProfile()));
+            config.AssertConfigurationIsValid();
+        }
+    }
+}
