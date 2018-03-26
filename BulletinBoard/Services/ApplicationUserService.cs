f161b67119c6515e1e1079b74b30e9cf2befe5d9
Krzysztof Kulewski <krzysztofkulewski@gmail.com>
2018-03-26 20:50:44 +0200

    Add app user service

---
+++ BulletinBoard/Services/ApplicationUserService.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using BulletinBoard.Data.Repositories.Abstract;
+using BulletinBoard.Models;
+using BulletinBoard.Services.Abstract;
+
+namespace BulletinBoard.Services
+{
+    public class ApplicationApplicationUserService : IApplicationUserService
+    {
+        private readonly IApplicationUserRepository _userRepository;
+
+        public ApplicationApplicationUserService(IApplicationUserRepository userRepository)
+        {
+            _userRepository = userRepository;
+        }
+
+        public async Task<IEnumerable<ApplicationUser>> GetAllUsers()
+        {
+            return await _userRepository.GetAll();
+        }
+    }
+}
